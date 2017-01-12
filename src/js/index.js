6f1ed379a63e2f9be450fa04f18f5119f170ea6a
Regueiro, Roberto <tarod_spj@hotmail.com>
2017-01-12 21:11:39 +0100

    haciendo funcionar los sonidos

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,29 @@
+var ping = new Audio("/android_asset/www/audio/Hi-Bongo.wav"),
+    endExercise = new Audio("/android_asset/www/audio/end.mp3");
+
+function countDown() {
+
+  var second = 10,
+      minute = 00;
+
+  setInterval(function(){
+    document.getElementById("timer").innerHTML = minute + ":" + second;
+    ping.play();
+    second--;
+
+    if(second == 00){
+      minute--;
+      second = 60;
+    }
+    if(minute == 0 && second == 1){
+      document.getElementById("timer").innerHTML = "Timer Stopped";
+      endExercise.play();
+    }
+    if(minute <= -1) {
+      document.getElementById("timer").innerHTML = " ";
+    }
+  }, 1000);
+}
 
 var app = {
     // Application Constructor
@@ -11,6 +37,8 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+
+
     },
 
     // Update DOM on a Received Event
@@ -19,10 +47,11 @@ var app = {
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
-        //listeningElement.setAttribute('style', 'display:none;');
+        listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+        countDown();
     }
 };
 
--- www/js/index.js
+++ www/js/index.js
@@ -9361,6 +9361,32 @@ return jQuery;
   }());
 
 }(jQuery, window));
+var ping = new Audio("/android_asset/www/audio/Hi-Bongo.wav"),
+    endExercise = new Audio("/android_asset/www/audio/end.mp3");
+
+function countDown() {
+
+  var second = 10,
+      minute = 00;
+
+  setInterval(function(){
+    document.getElementById("timer").innerHTML = minute + ":" + second;
+    ping.play();
+    second--;
+
+    if(second == 00){
+      minute--;
+      second = 60;
+    }
+    if(minute == 0 && second == 1){
+      document.getElementById("timer").innerHTML = "Timer Stopped";
+      endExercise.play();
+    }
+    if(minute <= -1) {
+      document.getElementById("timer").innerHTML = " ";
+    }
+  }, 1000);
+}
 
 var app = {
     // Application Constructor
@@ -9374,6 +9400,8 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+
+
     },
 
     // Update DOM on a Received Event
@@ -9382,10 +9410,11 @@ var app = {
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
-        //listeningElement.setAttribute('style', 'display:none;');
+        listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+        countDown();
     }
 };
 
