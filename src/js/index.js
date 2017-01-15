4fdfcea3dcfd4a3b6b674e1f30369e485c1d8157
Regueiro, Roberto <tarod_spj@hotmail.com>
2017-01-15 20:35:48 +0100

    funciona cuenta atras y sonido
    Las series aun no valen para nada
    Hay que quitar el foco a los input cuando le das a start
    Controlar que los minutos son 2 unidades... yo creo que si
    falta el Rest Time
    
    Por ahora esta en plan peyo esto hay que hacerlo con objetos y cosas bien...

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,88 @@
 var ping = new Audio("/android_asset/www/audio/Hi-Bongo.wav"),
     endExercise = new Audio("/android_asset/www/audio/end.mp3");
 
-function countDown() {
+var $wrapper, timer, continua = true;
+var seriesInicial, minutosInicial, segundosInicial;
+var series, minutos, segundos;
 
-  var second = 10,
-      minute = 00;
+function startTrainning() {
+  $wrapper = $('#wrapper');
+  seriesInicial = parseInt($wrapper.find('.numero_series').val());
+  minutosInicial = parseInt($wrapper.find('.minutos').val());
+  segundosInicial = parseInt($wrapper.find('.segundos').val());
+  minutos = minutosInicial;
+  segundos = segundosInicial;
+  series = seriesInicial;
+}
 
-  setInterval(function(){
-    document.getElementById("timer").innerHTML = minute + ":" + second;
+function restartTime() {
+  $wrapper.find('.minutos').val(minutosInicial);
+  $wrapper.find('.segundos').val(segundosInicial);
+}
+
+function restartTrainning() {
+  $wrapper.find('.numero_series').val(seriesInicial);
+  restartTime();
+}
+
+function updateSeconds() {
+  if(segundos <= 9) {
+    segundos = '0' + segundos;
+  }
+  $wrapper.find('.segundos').val(segundos);
+}
+
+function updateMinutes() {
+  if(minutos <= 9) {
+    minutos = '0' + minutos;
+  }
+  $wrapper.find('.minutos').val(minutos);
+}
+
+function updateTime() {
+
+  segundos = parseInt(segundos) - 1;
+
+  if (segundos < 0) {
+      segundos = 59;
+      if (minutos >= 1){
+        minutos = parseInt(minutos) - 1;
+        updateMinutes();
+      }
+      else {
+        continua = false;
+      }
+  }
+
+  if(continua){
+    updateSeconds();
     ping.play();
-    second--;
+    timer = setTimeout(updateTime, 1000);
+  } else {
+    clearTimeout(timer);
+    endExercise.play();
+  }
 
-    if(second == 00){
-      minute--;
-      second = 60;
+}
+
+function initializeApp() {
+  $('#botonEstado').on('click', function(){
+    var $this = $(this);
+    if($this.hasClass('play')){
+      $this.removeClass('play');
+      $this.addClass('stop');
+      startTrainning();
+      updateTime();
     }
-    if(minute == 0 && second == 1){
-      document.getElementById("timer").innerHTML = "Timer Stopped";
-      endExercise.play();
+    else {
+      $this.addClass('play');
+      $this.removeClass('stop');
+      clearTimeout(timer);
+      startTrainning();
     }
-    if(minute <= -1) {
-      document.getElementById("timer").innerHTML = " ";
-    }
-  }, 1000);
+  });
+
+
 }
 
 var app = {
@@ -37,8 +97,6 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
-
-
     },
 
     // Update DOM on a Received Event
@@ -49,9 +107,8 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
+        initializeApp();
         console.log('Received Event: ' + id);
-        countDown();
     }
 };
 
